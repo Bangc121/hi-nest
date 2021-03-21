cce48f31537c37b3d55dc610e3c675b5f63172f6
kim junghwan <kimjunghwan@kimui-MacBookPro-2.local>
2021-03-21 12:25:49 +0900

    unit test

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -47,3 +47,13 @@ export class MoviesController {
     return this.moviesService.update(movieId, updateData);
   }
 }
+
+
+// 테스트와 관련된게 5개가있다.
+// jest가 spec.ts파일들을 찾아볼수있도록 설정되어있어
+// npm run test:cov -> 모든 spec.ts 파일들을 찾아서 몇 줄이 테스팅되었는지 알려준다.
+// 유닛 테스트 모든 function을 따로 테스트하는것
+// 두가지의 테스팅 종류 유닛테스트
+// 서비스에서 분리된 유닛을 테스트하는걸 말해
+// 또다른 하나는 end-to-end(e2e)인데 모든 시스템을 테스트
+// 유닛테스팅은 getall() function 하나를 테스트할때 사용
